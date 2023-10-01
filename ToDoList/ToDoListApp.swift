be3003b0f9445c526b58bef7019386abd3591030
Erastoh <erastudmugambi@gmail.com>
2023-10-01 11:52:07 +0300

    ToDo List

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Erastus Mugambi on 23/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
