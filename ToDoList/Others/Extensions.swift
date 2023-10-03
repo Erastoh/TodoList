44d872112057a0b89a77e7c6b1862227f286b13c
Erastoh <erastudmugambi@gmail.com>
2023-10-03 15:31:47 +0300

    Data Decoding

---
+++ ToDoList/Others/Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  Extensions.swift
+//  ToDoList
+//
+//  Created by Erastus Mugambi on 03/10/2023.
+//
+
+import Foundation
+
+extension Encodable{
+    func asDictionary()->[String:Any] {
+        guard let data = try? JSONEncoder().encode(self) else{
+            return [:]
+        }
+        do{
+            let json = try JSONSerialization.jsonObject(with: data) as? [String:Any]
+            return json ?? [:]
+        }catch{
+            return [:]
+        }
+    }
+}
+
+//read on encodable in swift
