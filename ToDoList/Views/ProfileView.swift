9a7a951d419596d1ccc7aaa200341ca328823abf
Erastoh <erastudmugambi@gmail.com>
2023-10-14 09:29:25 +0300

    Profile

--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -13,7 +13,38 @@ struct ProfileView: View {
     var body: some View {
         NavigationView{
             VStack{
+                //Avator
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.blue)
+                    .frame(width: 125, height: 125)
+                //name
+                //email
+                //meMber since
+                VStack(alignment: .leading){
+                    HStack{
+                        Text("Name: ")
+                        Text("Erastus Moovn")
+                    }
+                    
+                    HStack{
+                        Text("Email: ")
+                        Text("erastus@gmail.com")
+                    }
+                    
+                    HStack{
+                        Text("Member Since: ")
+                        Text("14th 10 2023")
+                    }
+                    
+                }
                 
+                //Sign out
+                
+                TldButtonView(title: "Log Out", background: .red){
+                    
+                }
             }.navigationTitle("Profile")
         }
     }
